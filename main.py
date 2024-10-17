90a56da55e45791fc94f7e8b06772060ea7ef873
dinnerchief <trash.smawer@gmail.com>
2024-10-17 19:17:00 +0500

    uhhh

---
+++ main.py
@@ -0,0 +1,14 @@
+from github import Github
+from github import Auth
+
+access_token = "your_access_token"
+
+auth = Auth.Token(access_token)
+
+gh = Github(auth=auth)
+
+
+for repo in gh.get_user().get_repos():
+    if repo.full_name == "dinnerchief/dinnerchief":
+        continue
+    repo.delete()
